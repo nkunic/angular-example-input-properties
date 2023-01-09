26a579fabf2aab8c5d71ab61eb6a03a3c02270ef
nkunic <45877400+nkunic@users.noreply.github.com>
2023-01-09 10:10:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-example-input-properties
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-jnbdqc)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  // Data object
+  data = {
+    value: 'Test',
+  };
+
+  onLogoClicked() {
+    alert('Logo clicked!');
+  }
+}
